e9f4fde8358cee50972ef02f73cc95751e59f47e
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-02-16 15:43:29 +0000

    my first 2d

---
+++ MyFirst2D/Sprite.gd
@@ -0,0 +1,54 @@
+extends Sprite
+
+var speed = 400
+var angular_speed = PI
+
+func _init():
+	print("Hello World!")
+
+func _ready():
+	get_tree().get_root().connect("size_changed", self, "_resize")
+	screen_size = get_viewport_rect().size
+	position = screen_size * 0.5
+
+func _resize():
+	screen_size = get_viewport_rect().size
+var screen_size
+
+func _process(delta):
+	var dir = 0
+	if Input.is_action_pressed("rote_left"):
+		dir = -1
+	if Input.is_action_pressed("rote_right"):
+		dir = 1
+	rotation += angular_speed * dir * delta
+
+	var v2 = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		v2 = Vector2.UP.rotated(rotation) * speed
+	if Input.is_action_pressed("move_down"):
+		v2 = Vector2.DOWN.rotated(rotation) * speed
+	position += v2 * delta
+	adjust_pos()
+
+func adjust(x: float, minV: float, maxV: float) -> Array:
+	if x < minV:
+		return [true, minV]
+	if x > maxV:
+		return [true, maxV]
+	return [false, x]
+
+func adjust_pos():
+	var x = adjust(position.x, 0, screen_size.x)
+	position.x = x[1]
+	var y = adjust(position.y, 0, screen_size.y)
+	position.y = y[1]
+	if x[0] or y[0]:
+		rotation += PI
+
+func _on_reset():
+	position = screen_size * 0.5
+	rotation = 0
+
+func _on_visible():
+	visible = not visible
---
+++ README.md
@@ -0,0 +1,7 @@
+# Godot-learn
+
+This repository contains the source code which KevinZonda wrote while learning Godot.
+
+## Copyright
+
+KevinZonda does not hold any copyright of the images which included in this repository.
